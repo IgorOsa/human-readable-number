32e0808f2ade9a341e6370e827dac623ba6caa52
Igor Osadchyi <igor@webio.net.ua>
2020-10-07 12:38:03 +0300

    feat: implement toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
-module.exports = function toReadable (number) {
-  
+const TEN = 10;
+const ONE_HUNDRED = 100;
+const ONE_THOUSAND = 1000;
+const ONE_MILLION = 1000000;
+const ONE_BILLION = 1000000000;
+const ONE_TRILLION = 1000000000000;
+const ONE_QUADRILLION = 1000000000000000;
+const MAX = 9007199254740992;
+
+const LESS_THAN_TWENTY = [
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+];
+
+const TENTHS_LESS_THAN_HUNDRED = [
+    'zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+];
+
+function toReadable(number, words = []) {
+    let remainder;
+    let word = [];
+
+    if (number === 0) {
+        return words.length === 0 ? 'zero' : words.join(' ');
+    }
+
+    if (number < 0) {
+        words.push('minus');
+        number = Math.abs(number);
+    }
+
+    if (number < 20) {
+        remainder = 0;
+        word = LESS_THAN_TWENTY[number];
+
+    } else if (number < ONE_HUNDRED) {
+        remainder = number % TEN;
+        word = TENTHS_LESS_THAN_HUNDRED[Math.floor(number / TEN)];
+
+        if (remainder) {
+            word += ' ' + LESS_THAN_TWENTY[remainder];
+            remainder = 0;
+        }
+
+    } else if (number < ONE_THOUSAND) {
+        remainder = number % ONE_HUNDRED;
+        word = toReadable(Math.floor(number / ONE_HUNDRED)) + ' hundred';
+
+    } else if (number < ONE_MILLION) {
+        remainder = number % ONE_THOUSAND;
+        word = toReadable(Math.floor(number / ONE_THOUSAND)) + ' thousand';
+
+    } else if (number < ONE_BILLION) {
+        remainder = number % ONE_MILLION;
+        word = toReadable(Math.floor(number / ONE_MILLION)) + ' million';
+
+    } else if (number < ONE_TRILLION) {
+        remainder = number % ONE_BILLION;
+        word = toReadable(Math.floor(number / ONE_BILLION)) + ' billion';
+
+    } else if (number < ONE_QUADRILLION) {
+        remainder = number % ONE_TRILLION;
+        word = toReadable(Math.floor(number / ONE_TRILLION)) + ' trillion';
+
+    } else if (number <= MAX) {
+        remainder = number % ONE_QUADRILLION;
+        word = toReadable(Math.floor(number / ONE_QUADRILLION)) + ' quadrillion';
+    }
+
+    words.push(word);
+
+    return toReadable(remainder, words);
 }
+
+module.exports = toReadable;
